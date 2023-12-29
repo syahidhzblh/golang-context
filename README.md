9a83b05639192197cb2caa623031f4e864a13e07
syahidhzblh <syahidhizbullah17@gmail.com>
2023-12-30 00:23:50 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Golang Context
+
+Context in Golang used for sending data request to process also signal.
+
+Context include:
+- Value
+- Cancel Signal
+- Timeout Signal
+- Deadline Signal
---
+++ context_cancel_test.go
@@ -0,0 +1,50 @@
+package golang_context
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+// Context with Cancel untuk case Goroutine Leak
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel() //Mengirim sinyal cancel ke context
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
